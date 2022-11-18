c42d7178b9780db7f4353a4678b042da8b9b7a7f
rl627 <45013044+rl627@users.noreply.github.com>
2022-11-18 11:17:27 -0500

    set up `polite` prism pull workflow

--- index.R
+++ index.R
@@ -1,4 +1,3 @@
-
 # 0. Setup ----------------------------------------------------------------
 {
   ## Dependencies
@@ -9,10 +8,67 @@
   ## Directory managment
   setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
   prism_set_dl_dir(path = "download")
+  
+  ## set longer time out
+  options(timeout=99999999)
+  
+}
+
+
+# 1. Pull Function   ------------------------------------------------------
+#' To avoild time out errors, we just do smaller indivudal pulls split up in to 5 day periods
+
+## 1.1 Split dates into 5 day periods --------------------------------------
+all_dates = seq(as.Date("2007-01-01"), as.Date("2019-12-31"), by="days")
+dates_split  = split(all_dates, ceiling(seq_along(all_dates)/3))
+
+
+# 1.2 Create pull combinations --------------------------------------------
+template = tibble(type = list( c( "ppt", "tmean", "tmin", "tmax", "tdmean", "vpdmin",  "vpdmax")),
+                  dates = dates_split_5) %>% 
+  rowwise() %>% 
+  mutate(minDate = dates[[1]],
+         maxDate = dates[[length(dates)]]) %>% 
+  select(-dates) %>% 
+  unnest(cols = type) %>% 
+  mutate(id = row_number(),
+         grouper = id  )
+
+## 1.3 Write function to pull daily prism based on date input + type input date ---------
+##' Error handling added to function.
+politely_get_prism_dailys =function(template_row){
+  message_tmp = paste("pull id:", template_row$id)
+  tryCatch(                       # Applying tryCatch
+    expr = {                      # Specifying expression
+      message(paste("Starting",message_tmp))
+      get_prism_dailys(
+        type =  template_row$type, 
+        minDate = template_row$minDate, 
+        maxDate = template_row$maxDate, 
+        keepZip = F)
+      output = template_row %>% mutate(status = "sucess")
+      message(paste("Sucessful",message_tmp))
+      
+    },
+    
+    error = function(e){},
+    warning = function(w){        # Specifying warning message
+      output = template_row %>% mutate(status = "timeout")
+      message("There was a timeout")
+    },
+    finally = {}
+  )
+  
+  return(output)
 }
 
 
 
-# 1. Pull Function   ------------------------------------------------------
-# Here we will develop a working function to pull data via prism
+## 1.4 Iterate over all combinations of type and dates ---------------------
+#' Note we may
+status_first_pass = template  %>%  
+  sample_n(20) %>% 
+  group_by(grouper) %>% 
+  group_modify(~politely_get_prism_dailys(.x))
+
 
