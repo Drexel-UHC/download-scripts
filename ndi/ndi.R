b2317d2d17e848b90408369fd20423bde5bb3104
rl627 <rl627@drexel.edu>
2022-12-02 11:38:29 -0500

    update ndi as per proejct needs

--- ndi/ndi.R
+++ ndi/ndi.R
@@ -11,57 +11,76 @@
 }
 
 
-
-# 1. Pull basic non-subgroup metrics  --------------------------------------------
-#'    There are four metrics without sub groups. THe rest will need more complex 
-#'    templates that specify combinations of population groups. Lets pull these
-#'    simple ones first. 
-#'  
-#'    
-
+# 1. Pull metrics --------------------------------------------
 
 ## 1.1 template ------------------------------------------------------------
 {
-  metrics_no_subgroups = c( 
+  metrics = c( 
     'gini',
     'messer',
     'krieger',
-    'powell_wiley'
+    'powell_wiley',
+    'anthopolos',
+    'bravo'
   )
   
-  template_basic = expand_grid(
-    metric = metrics_no_subgroups, 
+  template = expand_grid(
+    metric = metrics, 
     state = state.abb, 
-    year = 2017:2020) %>% 
+    year = 2019) %>% 
     mutate(uhc_id = paste(state,year,metric, sep = '-'),
            row = row_number())
 }
 
+
+
 ## 1.2.  Error robust pull function  --------------------------------------------
 politely_get_ndi_basic = function(template_row){
+  
   message_tmp = paste("pull uhc_id:", template_row$uhc_id)
-  tryCatch(                        
+  
+  tryCatch( 
+    
     expr = {                      
       message(paste("Starting",message_tmp))
-      ndi_output = get(template_row$metric)(
-        state = template_row$state, 
-        year = 2020)
-      output = ndi_output[[1]]%>% 
-        mutate(uhc_id = template_row$uhc_id) %>% 
-        select(uhc_id, everything())
+      
+      if (template_row$metric%in%c('bravo')){
+        ndi_output = get(template_row$metric)(
+          state = template_row$state, 
+          year = template_row$year,
+          subgroup = c("LtHS", "HSGiE", "SCoAD"))
+      } else if (template_row$metric%in%c('anthopolos')){
+        ndi_output = get(template_row$metric)(
+          state = template_row$state, 
+          year = template_row$year,
+          subgroup = "NHoLB")
+      } else {
+        ndi_output = get(template_row$metric)(
+          state = template_row$state, 
+          year = template_row$year)
+      }
+      
+      output = ndi_output[[1]] %>% mutate(year = template_row$year)
+      
+      # output %>% write_csv(paste0("download/",template_row$uhc_id,".csv"))
+      
       message(paste("Sucessful",message_tmp))
     },
+    
     error = function(e){
       output = template_row %>% mutate(status = "error")
       message(e)
       message("\n !!!!!!!!!!!! Error")
     },
+    
     warning = function(w){       
       output = template_row %>% mutate(status = "warning")
       message(w)
       message("\n ####There was a warning")
     },
+    
     finally = {}
+    
   )
   return(output)
 }
@@ -70,12 +89,27 @@ politely_get_ndi_basic = function(template_row){
 
 ## 1.3 Iterate basic pulls ---------------------
 #' Note we may need to repeat some of the timed out pull id's... see next section
-ndi_pull_1 = template  %>%  
-  sample_n(1) %>% 
-  group_by(row) %>% 
-  group_modify(~politely_get_ndi(.x)) %>% 
-  ungroup()
+
+pull_ndi_metric = function(metric){
+  template  %>% 
+    filter(metric == metric) %>% 
+    # sample_n(2) %>%
+    group_by(row) %>% 
+    group_modify(~politely_get_ndi_basic(.x)) %>% 
+    ungroup()
+}
+
+results = metrics %>% 
+  map(~pull_ndi_metric(.x)) %>% 
+  set_names(metrics)
 
 
-# 1.4 QC basic runs ------------------------------------
-#' write logic to check for failed runs and then repeat until completion.
+# 1.4 Write datasets ------------------------------------
+
+map2(results, metrics,
+     function(result, metric){
+       file_name = paste0("download/",metric,".csv")
+       result %>% write_csv(file_name)
+     })
+
+
